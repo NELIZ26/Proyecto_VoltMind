36b59c87be549c105dadb077026acf1e6c050b80
ConsutoraEnergy <ferleytobon.23@gmail.com>
2026-06-02 23:47:16 -0500

    Feat: Cálculo de tiempo extra y formato de nombre de instructor en dashboard

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 # main.py
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from routers import fichas, aprendices
+from routers import fichas, aprendices, sesiones
 
 app = FastAPI(title="VoltMind API")
 
@@ -16,3 +16,4 @@ app.add_middleware(
 
 app.include_router(fichas.router)
 app.include_router(aprendices.router)
+app.include_router(sesiones.router)
